70e82508278cdf4bd21ae0b6ed80a928939236bb
Viktor Bilonizhko <whinstler@gmail.com>
2023-10-06 23:29:01 +0300

    First version of application moved into GitHub

---
+++ catalog/tests/test_views.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from django.urls import reverse
+
+import datetime
+
+from django.utils import timezone
+from django.contrib.auth.models import User # Required to assign User as a borrower
+
+from catalog.models import Author
+
+import uuid
+
+from django.contrib.auth.models import Permission # Required to grant the permission needed to set a book as returned.
+
+class AuthorCreateTest(TestCase):
+    def setUp(self):
+        # Create two users
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
+        test_user1.save()
+        test_user2.save()
+        
+        # Give test_user2 permission to renew books.
+        permission = Permission.objects.get(name='Set book as returned')
+        test_user2.user_permissions.add(permission)
+        test_user2.save()
+
+        #Create an author
+        test_author = Author.objects.create(
+        first_name='John',
+        last_name='Smith',
+        date_of_birth='1965-02-14',
+        date_of_death='2006-02-14',
+        )
+        test_author.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('author-create'))
+        self.assertRedirects(response, '/accounts/login/?next=/catalog/author/create/')
+
+    def test_forbidden_if_logged_in_but_not_correct_permission(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('author-create'))
+        self.assertEqual(response.status_code, 403)
+"""
+    def test_can_mark_returned_uses_correct_template(self):
+        login = self.client.login(username='testuser2', password='2HJ1vRV0Z&3iD')
+        response = self.client.get(reverse('author-create'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser2')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'catalog/author_form.html')
+"""
